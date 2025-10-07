af3c178cf7ce2df090a88b6891af4cf7ff0c2c78
Daniel Krastev <danielkrastev01@gmail.com>
2025-10-07 12:44:12 +0300

    Update airPollutionAPI.js

--- src/api/airPollutionAPI.js
+++ src/api/airPollutionAPI.js
@@ -2,42 +2,50 @@ const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 const urlAPI = 'http://api.openweathermap.org/data/2.5/air_pollution';
 
 async function getCurrentPosition() {
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
 }
 
 async function fetchByCoords(latitude, longitude) {
-  const url = `${urlAPI}?lat=${latitude}&units=metric&lon=${longitude}&appid=${apiKey}`;
-  const res = await fetch(url);
-  if (!res.ok) throw new Error(`HTTP error: ${res.status}`);
-  return await res.json();
+    const url = `${urlAPI}?lat=${latitude}&units=metric&lon=${longitude}&appid=${apiKey}`;
+    const res = await fetch(url);
+    if (!res.ok) throw new Error(`HTTP error: ${res.status}`);
+    return await res.json();
 }
 
 async function fetchByCity(city) {
-  const url = `${urlAPI}?q=${city}&units=metric&lang=en&appid=${apiKey}`;
-  const res = await fetch(url);
-  if (!res.ok) throw new Error(`HTTP error: ${res.status}`);
-  return await res.json();
+    const geoRes = await fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
+    );
+    const geoData = await geoRes.json();
+    if (!geoData || geoData.length === 0) {
+        throw new Error("City not found");
+    }
+
+    const { lat, lon } = geoData[0];
+
+    const airPollution = await fetchByCoords(lat, lon);
+    return airPollution;
 }
 
 export default {
-  async getAirPollution(city) {
-    let weatherData = [];
-    try {
-      const pos = await getCurrentPosition();
-      const { latitude, longitude } = pos.coords;
-      weatherData = await fetchByCoords(latitude, longitude);
-    } catch (geoError) {
-      console.warn("Geolocation error, fallback to city:", geoError);
-      try {
-        weatherData = await fetchByCity(city);
-      } catch (fetchError) {
-        console.error("Error to fetch weather:", fetchError);
-        weatherData = null;
-      }
-    }
-    
-    return weatherData;
-  },
+    async getAirPollution(city) {
+        let weatherData = [];
+        try {
+            const pos = await getCurrentPosition();
+            const { latitude, longitude } = pos.coords;
+            weatherData = await fetchByCoords(latitude, longitude);
+        } catch (geoError) {
+            console.warn("Geolocation error, fallback to city:", geoError);
+            try {
+                weatherData = await fetchByCity(city);
+            } catch (fetchError) {
+                console.error("Error to fetch weather:", fetchError);
+                weatherData = null;
+            }
+        }
+
+        return weatherData;
+    },
 };
