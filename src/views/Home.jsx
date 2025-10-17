86a4544157a25acaeee00e4153af500436e2f5c4
Daniel Krastev <danielkrastev01@gmail.com>
2025-10-17 22:10:23 +0300

    Update Home backgroud basic

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -45,48 +45,64 @@ export default function Home() {
     if (loading || !weather || weather.cod !== 200) return <p className="text-center mt-10">Loading...</p>;
 
     return (
-        <div className="text-white bg-teal-300 min-h-screen p-2 md:p-4">
-            <Header weather={weather} />
+        <>
+            <div className="relative min-h-screen overflow-hidden">
+                {/* background video */}
+                <video
+                    autoPlay
+                    loop
+                    muted
+                    playsInline
+                    className="absolute top-0 left-0 w-full h-full object-cover z-0"
+                    src={`./videos/sunset-beams.mp4`}
+                />
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                <div className="absolute inset-0 bg-black/30 z-10" />
 
-                <div className="col-span-1 lg:col-span-3 md:col-span-4 order-first">
-                    <HourlyForecast hourlyWeather={hourlyWeather || []} />
+                <div className="relative z-20 text-white p-2 md:p-4">
+                    <Header weather={weather} />
+
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+
+                        <div className="col-span-1 lg:col-span-3 md:col-span-4 order-first">
+                            <HourlyForecast hourlyWeather={hourlyWeather || []} />
+                        </div>
+
+                        <div className="border lg:row-span-3 lg:col-span-1 order-last lg:order-none h-auto min-h-[300px] max-h-[590px] md:col-span-2 md:order-none">
+                            <WeatherMap />
+                        </div>
+
+                        <div className="order-none lg:row-span-3 lg:col-span-1 lg:order-none md:col-span-2 md:order-none">
+                            <DailyForecast dailyForecast={dailyForecast || []} />
+                        </div>
+
+                        <div className="col-span-1 md:col-span-2 lg:col-span-1 order-none lg:order-none">
+                            <UviIndex uviIndex={uviIndex} />
+                        </div>
+
+                        <div className="col-span-1">
+                            <Humidity weather={weather} />
+                        </div>
+
+                        <div className="xxl:col-span-1 lg:col-span-2">
+                            <SunriseSunset weather={weather} />
+                        </div>
+
+                        <div className="col-span-1">
+                            <Visibility weather={weather} />
+                        </div>
+
+                        <div className="lg:col-span-1 md:col-span-2">
+                            <Pressure weather={weather} />
+                        </div>
+
+                        <div className="lg:col-span-1 xxl:col-span-2">
+                            <Wind wind={weather.wind || {}} airPollution={airPollution} />
+                        </div>
+
+                    </div>
                 </div>
-
-                <div className="border lg:row-span-3 lg:col-span-1 order-last lg:order-none h-auto min-h-[300px] max-h-[590px] md:col-span-2 md:order-none">
-                    <WeatherMap />
-                </div>
-
-                <div className="order-none lg:row-span-3 lg:col-span-1 lg:order-none md:col-span-2 md:order-none">
-                    <DailyForecast dailyForecast={dailyForecast || []} />
-                </div>
-
-                <div className="col-span-1 md:col-span-2 lg:col-span-1 order-none lg:order-none">
-                    <UviIndex uviIndex={uviIndex}/>
-                </div>
-
-                <div className="col-span-1">
-                    <Humidity weather={weather} />
-                </div>
-
-                <div className="xxl:col-span-1 lg:col-span-2">
-                    <SunriseSunset weather={weather} />
-                </div>
-
-                <div className="col-span-1">
-                    <Visibility weather={weather} />
-                </div>
-
-                <div className="lg:col-span-1 md:col-span-2">
-                    <Pressure weather={weather} />
-                </div>
-
-                <div className="lg:col-span-1 xxl:col-span-2">
-                    <Wind wind={weather.wind || {}} airPollution={airPollution} />
-                </div>
-
             </div>
-        </div>
+        </>
     );
 }
